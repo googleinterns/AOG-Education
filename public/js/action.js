462eb667c7fa7e6ecbfede56269e6f80a91b6e56
Sarah <sarah.cai@gmail.com>
2020-07-24 15:28:42 -0700

    fix formatting

--- public/js/action.js
+++ public/js/action.js
@@ -3,36 +3,37 @@
  * along with its callbacks.
  */
 export class Action {
-    /**
-     * @param {*} scene which serves as a container of all visual elements
-     */
-    constructor(scene) {
-      this.canvas = window.interactiveCanvas;
-      this.scene = scene;
-      this.commands = {
+  /**
+    * @param {*} scene which serves as a container of all visual elements
+    */
+  constructor(scene) {
+    this.canvas = window.interactiveCanvas;
+    this.scene = scene;
+    this.commands = {
+    };
 
-      };
-      this.commands.LOAD_MAP.bind(this);
-    }
-    /**
-     * Register all callbacks used by Interactive Canvas
-     * executed during scene creation time.
-     *
-     */
-    setCallbacks() {
-      // declare Interactive Canvas callbacks
-      const callbacks = {
-        onUpdate: (data) => {
-          try {
-            this.commands[data[0].command.toUpperCase()](data[0]);
-          } catch (e) {
-            // do nothing, when no command is sent or found
-          }
-        },
-      };
-      callbacks.onUpdate.bind(this);
-      // called by the Interactive Canvas web app once web app has loaded to
-      // register callbacks
-      this.canvas.ready(callbacks);
-    }
+    //   this.commands.RESTART_GAME.bind(this);
+    this.commands.LOAD_MAP.bind(this);
   }
+  /**
+    * Register all callbacks used by Interactive Canvas
+    * executed during scene creation time.
+    *
+    */
+  setCallbacks() {
+    // declare Interactive Canvas callbacks
+    const callbacks = {
+      onUpdate: (data) => {
+        try {
+          this.commands[data[0].command.toUpperCase()](data[0]);
+        } catch (e) {
+          // do nothing, when no command is sent or found
+        }
+      },
+    };
+    callbacks.onUpdate.bind(this);
+    // called by the Interactive Canvas web app once web app has loaded to
+    // register callbacks
+    this.canvas.ready(callbacks);
+  }
+}
