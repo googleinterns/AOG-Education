7930435ec6a39006a9b6491c6400c4133ccda934
Gaurnett Flowers <flowersgaurnett@gmail.com>
2020-08-26 11:18:48 -0500

    Added animations

--- public/js/scene.js
+++ public/js/scene.js
@@ -24,6 +24,7 @@ export class Scene {
 
     constructor() {
         this.game.appendChild(this.menu.getMenu());
+        this.createAnimation();
     }
 
     aogOpenMainMenu() {
@@ -32,16 +33,19 @@ export class Scene {
     }
 
     openGeography() {
+        document.getElementById("animation-canvas").style.visibility = "hidden";
         this.game.removeChild(this.menu.getMenu());
         this.game.appendChild(this.geography.getGeographyElement());
     }
 
     openLanguage() {
+        document.getElementById("animation-canvas").style.visibility = "visible";
         this.game.removeChild(this.menu.getMenu());
         this.game.appendChild(this.language.getLanguageElement());
     }
 
     openReading() {
+        document.getElementById("animation-canvas").style.visibility = "hidden";
         this.game.style.display = "flex";
         this.game.style.alignItems = "center";
         this.game.removeChild(this.menu.getMenu());
@@ -386,8 +390,11 @@ export class Scene {
         this.game.appendChild(this.language.getLanguageElement());
     }
 
-    createCanvas() {
-        // Initialize rendering and set correct sizing.
+    /**
+     * Creates the background animation
+     */
+    createAnimation() {
+        // initialize rendering and set correct sizing
         this.ratio = window.devicePixelRatio;
         this.renderer = PIXI.autoDetectRenderer({
             transparent: true,
@@ -399,6 +406,75 @@ export class Scene {
         this.element = this.renderer.view;
         this.element.style.width = `${this.renderer.width / this.ratio}px`;
         this.element.style.height = `${this.renderer.height / this.ratio}px`;
+        this.element.id = "animation-canvas"
         this.game.appendChild(this.element);
+
+        // center stage and normalize scaling for all resolutions
+        this.stage = new PIXI.Container();
+        this.stage.position.set(
+            this.game.clientWidth / 2,
+            this.game.clientHeight / 2
+        );
+        this.stage.scale.set(
+            Math.max(this.renderer.width, this.renderer.height) / 1024
+        );
+
+        var boxes = [];
+        boxes.push(this.drawShape("rect", "0x4285F4", -280, -150, 200, 200));
+        boxes.push(this.drawShape("circle", "0xDB4437", 280, -180, 110, 0));
+        boxes.push(this.drawShape("rect", "0x0F9D58", 230, 120, 220, 140));
+        boxes.push(this.drawShape("line", "0xF4B400", -240, 70, 125, 90));
+
+        let last = performance.now();
+        // frame-by-frame animation function
+        const frame = () => {
+            // calculate time differences for smooth animations
+            const now = performance.now();
+            const delta = now - last;
+
+            for (var index = 0; index < boxes.length; index++) {
+                index % 2 == 0
+                    ? (boxes[index].rotation -= delta / 1000)
+                    : (boxes[index].rotation += delta / 1000);
+            }
+
+            last = now;
+
+            this.renderer.render(this.stage);
+            requestAnimationFrame(frame);
+        };
+        frame();
+    }
+
+    /**
+     *
+     * @param {*} type of shape to be drawn
+     * @param {*} color of the shape's fill
+     * @param {*} x coordinate of the shape
+     * @param {*} y coordinate of the shape
+     * @param {*} width of the shape
+     * @param {*} height of the shape
+     */
+    drawShape(type, color, x, y, width, height) {
+        var shape = new PIXI.Graphics();
+        shape.beginFill(color);
+        if (type == "circle") {
+            shape.drawCircle(50, 50, width);
+        } else if (type == "rect") {
+            shape.drawRect(0, 0, width, height);
+        } else if (type == "ellipse") {
+            shape.drawEllipse(25, 25, width, height);
+        } else if (type == "line") {
+            shape.lineStyle(10, color, 1);
+            shape.moveTo(5, 0);
+            shape.lineTo(5, 250);
+        }
+
+        shape.endFill();
+        shape.x = x;
+        shape.y = y;
+        this.stage.addChild(shape);
+
+        return shape;
     }
 }
