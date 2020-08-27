e06d8c9521e35d1bceb18cc607b50d8c9a181fa4
Sami <BlueBubbleBeam@outlook.com>
2020-08-26 23:49:47 -0400

    style tweaks

--- public/js/dist/config.dev.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-
-Object.defineProperty(exports, "__esModule", {
-  value: true
-});
-exports.MAPS_API_KEY = void 0;
-// REPLACE WITH API KEY
-var MAPS_API_KEY = "AIzaSyCEkJ_bdDaUhX4pUioSZrAwpuUDdtM8xrw";
-exports.MAPS_API_KEY = MAPS_API_KEY;
